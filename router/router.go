1358cda1b8cd52807803ec159e587a2cf4bd2530
the-dg04 <devgup04@gmail.com>
2024-09-23 01:12:39 +0530

    added mail route

--- router/router.go
+++ router/router.go
@@ -11,14 +11,14 @@ import (
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api", logger.New())
 	api.Get("/", handler.Hello)
-
+	api.Get("/mail", handler.SendMail)
 	user := api.Group("/user")
 	user.Post("/create", handler.CreateUser)
 	user.Post("/login", handler.Login)
-	user.Post("/updatePassword",handler.UpdatePassword)
-	user.Post("/checkUsername",handler.CheckIfUsernameExists)
-	user.Post("/checkGmail",handler.CheckIfGmailExists)
-	user.Post("/checkGithub",handler.CheckIfGithubExists)
-	user.Post("/recoverPassword",handler.PasswordRecovery)
-	user.Post("/updatePassword",handler.UpdatePassword)
+	user.Post("/updatePassword", handler.UpdatePassword)
+	user.Post("/checkUsername", handler.CheckIfUsernameExists)
+	user.Post("/checkGmail", handler.CheckIfGmailExists)
+	user.Post("/checkGithub", handler.CheckIfGithubExists)
+	user.Post("/recoverPassword", handler.PasswordRecovery)
+	user.Post("/updatePassword", handler.UpdatePassword)
 }
