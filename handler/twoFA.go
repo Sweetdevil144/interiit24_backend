eaee0e21e5474108da27148fb1aa27d460254b2e
the-dg04 <devgup04@gmail.com>
2024-09-23 14:29:00 +0530

    added otp generation

--- handler/twoFA.go
+++ handler/twoFA.go
@@ -1,20 +1,29 @@
 package handler
 
 import (
-	"strconv"
 	"math/rand"
+	"server/database"
+	"server/model"
+	"strconv"
+	"time"
 )
 
 func GenerateOTP() string {
-	otp:=""
-	for i:=0;i<4;i++ {
-		otp+=strconv.Itoa(rand.Intn(10))
+	otp := ""
+	for i := 0; i < 4; i++ {
+		otp += strconv.Itoa(rand.Intn(10))
 	}
 	return otp
 }
 
-func TwoFA(tempToken string) error {
-	// otp:=GenerateOTP()
-	return nil
+func InsertOrUpdateOTP(tempToken, otp string) error {
+	db := database.DB
+	err := db.Where(model.OtpQueue{TempToken: tempToken}).
+		Assign(model.OtpQueue{TempToken: tempToken, Otp: otp, ExpiresAt: time.Now().Add(10 * time.Minute)}).
+		FirstOrCreate(&model.OtpQueue{}).Error
+	return err
+}
 
+func TwoFA(tempToken string) error {
+	return InsertOrUpdateOTP(tempToken, GenerateOTP())
 }
--- model/OtpQueue.go
+++ model/OtpQueue.go
@@ -1,10 +1,15 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // User struct
 type OtpQueue struct {
 	gorm.Model
 	TempToken string `gorm:"uniqueIndex;not null" json:"temp_token"`
 	Otp    string `gorm:"not null" json:"otp"`
+	ExpiresAt time.Time `gorm:"not null" json:"expires_at"`
 }
