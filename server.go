62de893efb0b9db25c8d4f5bf902e24da3818644
the-dg04 <devgup04@gmail.com>
2024-09-24 22:31:12 +0530

    added LRU in-memory caching

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"server/database"
 	"server/router"
+	"server/cache"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -12,6 +13,7 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 	database.ConnectDB()
+	cache.Init()
 	router.SetupRoutes(app)
 	log.Fatal(app.Listen(":3000"))
 }
