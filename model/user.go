3fbc314a99f5b24620535c026341ef2d4d6b92b9
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-09-24 23:04:46 +0530

    Update null index

--- model/user.go
+++ model/user.go
@@ -8,5 +8,5 @@ type User struct {
 	Gmail    string `gorm:"uniqueIndex;not null" json:"gmail"`
 	Github    string `gorm:"uniqueIndex" json:"github"`
 	Password string `gorm:"not null" json:"password"`
-	Name    string `json:"name"`
+	Name    string `gorm:"not null" json:"name"`
 }
