a602a95e3810a09c4526deaccc915c24afa1575b
the-dg04 <devgup04@gmail.com>
2024-10-07 22:09:44 +0530

    cors fix

--- compute_server/server.go
+++ compute_server/server.go
@@ -12,11 +12,12 @@ import (
 
 func main() {
 	app := fiber.New()
-	app.Use(cors.New(
-		cors.Config{
-			AllowOrigins: "http://localhost:3000",
-			AllowMethods: "GET,POST,PUT,DELETE",
-		}))
+	app.Use(cors.New())
+	// app.Use(cors.New(
+	// 	cors.Config{
+	// 		AllowOrigins: "http://localhost:3000",
+	// 		AllowMethods: "GET,POST,PUT,DELETE",
+	// 	}))
 	database.ConnectDB()
 	cache.Init()
 	router.SetupRoutes(app)
