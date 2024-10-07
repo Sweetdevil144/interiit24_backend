3bf84d7f80b6514a6ad216d07088e50653088b1c
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-10-07 10:56:57 +0530

    Update package installation in prod
    Signed-off-by: Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>

--- nginx_control.sh
+++ nginx_control.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Check if nginx is installed, and if not, install it (only for Linux)
+if [[ "$(uname)" == "Linux" ]]; then
+  if ! command -v nginx &> /dev/null; then
+    echo "Nginx not found. Installing Nginx..."
+    sudo apt-get update && sudo apt-get install -y nginx
+  fi
+fi
+
 command="$1"
 if [[ $command == "start" ]]; then
   nginx -c $(pwd)/nginx/nginx.conf && echo "Started Service"
